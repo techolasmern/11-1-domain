9dcc7a050659baa57f50c6c6a6ffd45eeb02c164
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-09 11:34:35 +0530

    operators

---
+++ JavaScript/09-06-2026/script.js
@@ -0,0 +1,40 @@
+console.log("Hi")
+
+// Operator
+
+// Assignment Operator (=)
+
+const pi = 3.14;
+console.log(pi);
+
+// Arithmetic Operator (+, -, *, /, %, **)
+
+// + -> addition
+// - -> subtraction
+// * -> multiplication
+// / -> division
+// % -> modulus (remainder)
+// ** -> exponentiation (power)
+
+console.log(1 + 1);
+console.log("Hello" + " " + "World");
+console.log(1 + "1"); // number + string -> string
+console.log("1" + 1); // string + number -> string
+console.log(1 + +"1"); // number + number -> number
+console.log("1" + +"1"); // string + number -> string
+console.log("1" + +1); // string + number -> string
+
+console.log(2 - 1);
+console.log("2" - 1); // string - number -> number
+console.log(2 - "1"); // number - string -> number
+// do rest of the operators
+
+console.log(2 * 2);
+
+console.log(2 / 2);
+
+console.log(6473 % 10);
+console.log(6473 / 100);
+
+console.log(2 ** 2);
+console.log(2 ** 3);
