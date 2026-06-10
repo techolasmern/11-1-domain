0ba41faabbfefbe801d9e34f201aff9ea8345be2
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-10 12:36:16 +0530

    samples

--- JavaScript/10-06-2026/script.js
+++ JavaScript/10-06-2026/script.js
@@ -45,3 +45,36 @@ console.log(vol);
 // console.log(area * 60);
 // const ar = getArea(100, 200);
 // console.log(ar);
+
+// even or odd
+
+// data types -> number, string, boolean, object, undefined,
+// 
+
+function isOdd(n){
+    const r = n % 2;
+    return Boolean(r);
+}
+
+const res = isOdd(16);
+console.log(res);
+
+// sum of n number
+
+// (n * (n + 1))/2
+
+function sumOfNNumbers(n) {
+    const s = (n * (n + 1)) / 2;
+    console.log(s)
+}
+
+sumOfNNumbers(100);
+
+// calculator
+
+function add(a, b) {
+    const s = a + b;
+    return s;
+}
+
+//  p/n * 100 -> find % of a number
