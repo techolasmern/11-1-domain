e58292cdd4b01847c22fa5c021020386886008a8
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-11 11:58:40 +0530

    if, if else, else if

--- JavaScript/11-06-2026/script.js
+++ JavaScript/11-06-2026/script.js
@@ -15,3 +15,37 @@ console.log(1 < -1 || 7 <= 3); // => false || false => false;
 
 console.log(!null); 
 // false value -> undefind, null, 0, ""
+
+// if, else, if else -> control statements
+
+if (10 < 5) {
+    // statement
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// 
+const a = 100;
+const b = 20;
+
+if (a < b) {
+    console.log("a is less than b");
+} else {
+    console.log("a is not less than b");
+}
+
+const c = 100;
+const d = 200;
+const e = 30;
+
+if (c > d && c > e) {
+    console.log("c is greater value");
+} else if (d > e) {
+    console.log("d is greater value");   
+} else {
+    console.log("e is greater value");
+}
+
+
+
