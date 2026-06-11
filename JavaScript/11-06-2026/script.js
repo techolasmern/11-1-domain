589e4ffd7a2db954f434559eb371fd684c617a85
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-11 12:31:29 +0530

    conditional statement with function

--- JavaScript/11-06-2026/script.js
+++ JavaScript/11-06-2026/script.js
@@ -16,12 +16,14 @@ console.log(1 < -1 || 7 <= 3); // => false || false => false;
 console.log(!null); 
 // false value -> undefind, null, 0, ""
 
-// if, else, if else -> control statements (Conditional statement)
+// if, else, if 
+// -> control statements (Conditional statement)
 
 if (10 < 5) {
     // statement
     console.log(true);
-} else {
+} 
+{
     console.log(false);
 }
 
@@ -31,7 +33,8 @@ const b = 20;
 
 if (a < b) {
     console.log("a is less than b");
-} else {
+} 
+{
     console.log("a is not less than b");
 }
 
@@ -41,11 +44,76 @@ const e = 30;
 
 if (c > d && c > e) {
     console.log("c is greater value");
-} else if (d > e) {
+} 
+if (d > e) {
     console.log("d is greater value");   
-} else {
+} 
+{
     console.log("e is greater value");
 }
 
+// SUN - SAT
+// 1 - 7
 
+// function getDay(n) {
+//     if (n == 1) {
+//         return "Sunday";
+//     } 
+//     if (n == 2) {
+//         return "Monday"
+//     } 
+//     if (n == 3) {
+//         return "Tuesday";
+//     } 
+//     if (n == 4) {
+//         return "Wednesday"
+//     } 
+//     if (n == 5) {
+//         return "Thursday";
+//     } 
+//     if (n == 6) {
+//         return "Friday";
+//     } 
+//     if (n == 7) {
+//         return "Saturday";
+//     } 
+//     return "Invalid input";
+// }
 
+function getDay(n) {
+    if (n == 1) {
+        return "Sunday";
+    } else if (n == 2) {
+        return "Monday"
+    } else if (n == 3) {
+        return "Tuesday";
+    } else if (n == 4) {
+        return "Wednesday"
+    } else if (n == 5) {
+        return "Thursday";
+    } else if (n == 6) {
+        return "Friday";
+    } else if (n == 7) {
+        return "Saturday";
+    } else {
+        return "Invalid input";
+    }
+}
+
+const res = getDay(8);
+console.log(res)
+
+// calculator
+
+function calc(fn, op, sn) {
+    if (op == "+") {
+        return fn + sn;
+    }
+    if (op == "/") {
+        return fn / sn;
+    }
+    // complete the program
+}
+
+const s = calc(10, "/", 30);
+console.log(s);
