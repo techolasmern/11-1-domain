29036bfc14ca15091415cf5685cf80b6e8b80802
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-12 12:12:27 +0530

    loop q1

--- JavaScript/12-06-2026/script.js
+++ JavaScript/12-06-2026/script.js
@@ -6,7 +6,6 @@ const str = "Hello World";
 // index starting from 0
 
 const val = str[6];
-
 console.log(str.length); // 11 -> lastIndex - 10
 
 // Array => Collection of elements or data;
@@ -35,7 +34,28 @@ console.log(value);
 for (let k = 1; k <= 10; k++){
     console.log(k);
 }
-console.log("first")
+console.log("first");
 
 
+// sum of N Numbers;
 
+function sumOfNNumbers(n) {
+    let sum = 0;
+    for (let j = 1; j <= n; j++){
+        sum = sum + j;
+        // Value of j is 1, then the sum = 0 + 1 (sum will be updated to 1)
+        // Value of j is 2, then the sum = 1 + 2 (sum will be updated to 3)
+        // Value of j is 3, then the sum = 3 + 3 (sum will be updated to 6)
+        // Value of j is 4, then the sum = 6 + 4 (sum will be updated to 10)
+        // Value of j is 5, then the sum = 10 + 5 (sum will be updated to 15)
+        // Value of j is 6, then the sum = 15 + 6 (sum will be updated to 21)
+        // Value of j is 7, then the sum = 21 + 7 (sum will be updated to 28)
+        // Value of j is 8, then the sum = 28 + 8 (sum will be updated to 36)
+        // Value of j is 9, then the sum = 36 + 9 (sum will be updated to 45)
+        // Value of j is 10, then the sum = 45 + 10 (sum will be updated to 55)
+    }
+    return sum;
+}
+
+const res = sumOfNNumbers(100);
+console.log(res);
