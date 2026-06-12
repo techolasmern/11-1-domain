10a0fe611efd348e1f0fc5e007a0abd757ad939a
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-12 11:41:38 +0530

    string, array base and for loop

---
+++ JavaScript/12-06-2026/script.js
@@ -0,0 +1,41 @@
+// String, Array, for loop
+
+// STRING => Collection of char.
+
+const str = "Hello World";
+// index starting from 0
+
+const val = str[6];
+
+console.log(str.length); // 11 -> lastIndex - 10
+
+// Array => Collection of elements or data;
+
+const data = 10;
+const numbers = [10, "hello", true];
+
+const value = numbers[1];
+
+console.log(value);
+
+// loop -> loop is used for repeating the same code block until the condition is false;
+
+// for loop, while, do while, for in, for of
+
+// for loop;
+
+// for (init; condn; update;) {
+//     // code
+// }
+
+// Sync - line by line execution of code.
+// Async - Parallel execution at a time.
+
+// 1 - 10 print
+for (let k = 1; k <= 10; k++){
+    console.log(k);
+}
+console.log("first")
+
+
+
