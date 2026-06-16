172c1b60a183016ae80bb35762c79aa1b27c734d
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-16 12:17:48 +0530

    palindrome

--- JavaScript/16-06-2026/script.js
+++ JavaScript/16-06-2026/script.js
@@ -12,13 +12,24 @@ while (val < 10) { // condn
     val++ // updation
 }
 
-// if (10 < 20) {
-//     console.log("first")
-// }
-
 // Exit controlled loop
 let i = 0; // init
 do {
     console.log(i); // statement
     i++; // updation
 } while (i < 0);
+
+function is_palindrome(number) {
+    let temp = number;
+    // temp = 123
+    let rev = 0;
+    while (temp > 0) { // 0 > 0
+        // reminder
+        const rem = temp % 10; // 1%10=1
+        rev = rev * 10 + rem; // 32*10+1=321
+        temp = parseInt(temp / 10); // 1/10=0;
+    }
+    return rev == number;
+}
+
+console.log(is_palindrome(123))
