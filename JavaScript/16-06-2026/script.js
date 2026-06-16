373af5e0345399079283dd5d2d0c7b5cdda0e941
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-16 11:36:06 +0530

    while and do while loop

---
+++ JavaScript/16-06-2026/script.js
@@ -0,0 +1,24 @@
+// loop
+
+// for loop
+// while, do while, for in, for of
+// Array -> collection of elements
+const arr = [1, 2, 3, 4, 5];
+
+// Entry controlled loop
+let val = 0; // init
+while (val < 10) { // condn
+    console.log(val); // statement
+    val++ // updation
+}
+
+// if (10 < 20) {
+//     console.log("first")
+// }
+
+// Exit controlled loop
+let i = 0; // init
+do {
+    console.log(i); // statement
+    i++; // updation
+} while (i < 0);
