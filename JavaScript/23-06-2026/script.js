ba31c456476dae57ce0dd683c4a515764462b2d4
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-23 12:24:16 +0530

    some, every, find, reduce

--- JavaScript/23-06-2026/script.js
+++ JavaScript/23-06-2026/script.js
@@ -32,4 +32,41 @@ console.log(newArray);
 const resArr = arr.map(element => element % 5 == 0 ? element * 10 : element);
 console.log(resArr);
 
+// some, every, find, reduce
+
+const res = arr.some(num => num % 10 == 0);
+console.log(res);
+
+const res2 = arr.every(num => num % 1 == 0);
+console.log(res2);
+
+const res3 = arr.find(num => num % 5 == 0);
+console.log(res3);
+
+// returns a single value
+
+let sum = 0;
+for (let i = 1; i <= 10; i++){
+    sum += i;
+}
+console.log(sum);
+
+const result = arr.reduce((prev, num) => {
+    return prev * num;
+}, 1);
+
+console.log(result);
+
+// 1 => prev: 0 => 0 + 1 = 1
+// 2 => prev: 1 => 1 + 2 = 3
+// 3 => prev => 3 => 3 + 3 = 6
+// 4 - 10, 15, 21, 45, 55
+
+const strArr = ["Amal", "Aswin", "Rahul", "AMaya"];
+
+const names = strArr.filter(name => name.toLowerCase().startsWith("am"));
+console.log(names);
+
+const updatedNames = strArr.map(name => name.toUpperCase());
+console.log(updatedNames);
 
