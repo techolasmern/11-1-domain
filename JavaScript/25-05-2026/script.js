b6415b10103f2becfb9ba043b40fcc938b36b5e2
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-25 12:55:23 +0530

    cleanup

--- JavaScript/25-05-2026/script.js
+++ JavaScript/25-05-2026/script.js
@@ -1,101 +1,101 @@
-// // OBJECTS, MATH & DATE
+// OBJECTS, MATH & DATE
 
-// // Object -> collection of key-value pairs.
+// Object -> collection of key-value pairs.
 
-// // let name = "amal";
-// // const age = 20;
-// // const address = "sfsdf"
+// let name = "amal";
+// const age = 20;
+// const address = "sfsdf"
 
-// // name = "aswin";
+// name = "aswin";
 
-// // console.log(name);
+// console.log(name);
 
-// // [10, 20, ]
-// const obj = {
-//     name: "Ajith",
-//     age: 25
-// }
+// [10, 20, ]
+const obj = {
+    name: "Ajith",
+    age: 25
+}
 
-// const students = [{ name: "Ajith" }, { name: "Rahul", age: 20 }, { name: "Aswin" }];
+const students = [{ name: "Ajith" }, { name: "Rahul", age: 20 }, { name: "Aswin" }];
 
-// console.log(students[0].name);
+console.log(students[0].name);
 
-// const object = {
-//     name: "Jinu",
-//     age: 21,
-//     address: {
-//         town: "Palazhi",
-//         city: "Kozhikode",
-//         state: "Keralam"
-//     },
-//     skills: ["HTML", "CSS", "REACT", "PYTHON", "NODE"]
-// }
+const object = {
+    name: "Jinu",
+    age: 21,
+    address: {
+        town: "Palazhi",
+        city: "Kozhikode",
+        state: "Keralam"
+    },
+    skills: ["HTML", "CSS", "REACT", "PYTHON", "NODE"]
+}
 
-// object.address.city = "Thrissur";
+object.address.city = "Thrissur";
 
-// // const key = "city";
-// // console.log(object.address[key]);
-// // const st = object["address"]["state"];
-// // console.log(st);
+// const key = "city";
+// console.log(object.address[key]);
+// const st = object["address"]["state"];
+// console.log(st);
 
-// console.log(object);
+console.log(object);
 
-// console.log(object.address.city);
-// console.log(object.skills);
+console.log(object.address.city);
+console.log(object.skills);
 
-// // Object Methods
+// Object Methods
 
-// // Object.keys(), Object.values(), Object.entries(), Object.freeze(), Object.seal(), Object.hasOwn()
+// Object.keys(), Object.values(), Object.entries(), Object.freeze(), Object.seal(), Object.hasOwn()
 
-// const user = {
-//     first_name: "Rathul",
-//     last_name: "Dev",
-//     age: 25,
-//     address: {
-//         city: "Kozhikode",
-//         state: "Keralam"
-//     }
-// }
+const user = {
+    first_name: "Rathul",
+    last_name: "Dev",
+    age: 25,
+    address: {
+        city: "Kozhikode",
+        state: "Keralam"
+    }
+}
 
 
-// console.log(Object.hasOwn(user.address, "state"));
+console.log(Object.hasOwn(user.address, "state"));
 
-// console.log(Object.values(user));
-// console.log(Object.keys(user));
-// console.log(Object.entries(user)); // [ [key, value], [key, value], [key, value] ];
+console.log(Object.values(user));
+console.log(Object.keys(user));
+console.log(Object.entries(user)); // [ [key, value], [key, value], [key, value] ];
 
-// // Object.freeze(user); // Can't add or modify fields.
-// Object.seal(user); // Can't add fields but can modify
+// Object.freeze(user); // Can't add or modify fields.
+Object.seal(user); // Can't add fields but can modify
 
-// user.skjhkdf = 90;
-// user.age = 30;
+user.skjhkdf = 90;
+user.age = 30;
 
-// console.log(user);
+console.log(user);
 
-// // Date
+// Date
 
-// // const current_time = Date.now(); // in milliseconds
+// const current_time = Date.now(); // in milliseconds
 
-// const date = new Date();
+const date = new Date();
 
-// console.log(date.getDate());
-// console.log(date.getDay());
-// console.log(date.getFullYear());
-// console.log(date.getHours());
-// console.log(date.getMinutes());
-// console.log(date.getSeconds());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
 
-// console.log(date.getMonth() + 1);
-// console.log(date.getTime());
-// console.log(date.toLocaleString("en-IN", {
-//     // dateStyle: "medium"
-//     day: "2-digit",
-//     month: "long",
-//     hour12: false,
-//     hour: "2-digit",
-//     minute: "2-digit",
-//     second: "2-digit"
-// }))
+console.log(date.getMonth() + 1);
+console.log(date.getTime());
+console.log(date.toLocaleString("en-IN", {
+    // dateStyle: "medium"
+    day: "2-digit",
+    month: "long",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+}))
 
 // // Math
 
