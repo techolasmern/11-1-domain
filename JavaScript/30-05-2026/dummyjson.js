57eb87c9de3826657eff8a7ea7cff662e6f4e85a
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-30 12:39:31 +0530

    api calling - dummyjson

---
+++ JavaScript/30-05-2026/dummyjson.js
@@ -0,0 +1,41 @@
+
+// const a = undefined ?? "skdhjfsdf";
+// console.log(a);
+
+const BASE_URL = "https://dummyjson.com";
+
+// GET, POST, PATCH, PUT, DELETE
+
+const request = async (api, config) => {
+    try {
+        const response = await fetch(api, config);
+        const res = await response.json();
+        return res;
+    } catch (e) {
+        return { error: e.message ?? "Request failed!" }
+    }
+}
+
+const getAllProducts = async () => {
+    try {
+        const response = await request(BASE_URL + "/products")
+        console.log(response);
+    } catch (e) {
+        return e
+    }
+}
+
+const addProduct = async (product) => {
+    try {
+        const response = await request(BASE_URL + "/products/add", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(product)
+        })
+        console.log(response);
+    } catch (e) {
+        return e;
+    }
+}
+
+addProduct({title: "sdkfhgskdhfkjsdf"})
