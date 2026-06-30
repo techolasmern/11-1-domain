b0b8b6279440335601d1c68f5f12c1056914cdf6
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-30 12:02:09 +0530

    api fetch

--- JavaScript/30-05-2026/script.js
+++ JavaScript/30-05-2026/script.js
@@ -46,3 +46,35 @@ const handleAsync = async () => {
 }
 
 handleAsync();
+
+
+// fetch -> get content from an api.
+
+const getApiData = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const res = await response.json();
+        console.log(res);
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+getApiData();
+
+// status code -> 100, 200, 300, 400, 500
+// 200, 400, 500 ->
+// 200 => OK
+// 201 => Created
+
+// 400 => Bad Request
+// 401 => Unauthorized
+// 404 => Not Found!
+// 409 => Conflict
+
+// 500 => Internal Server Error
+
+// object ->
+// JSON -> 
+// JSON -> JAVASCRIPT OBJECT NOTATION
+const obj = '{ "name": "amal", "age": 30 }';
