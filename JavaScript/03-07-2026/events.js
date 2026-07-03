3f9815d6c2b7af6c272affa0ea2c1edf732bde36
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-03 11:56:48 +0530

    custom context menu

--- JavaScript/03-07-2026/events.js
+++ JavaScript/03-07-2026/events.js
@@ -1,18 +1,26 @@
 const btn = document.getElementById("btn");
 
+const follower = document.querySelector(".follower");
+const customContextMenu = document.getElementById("custom-context-menu");
+
 btn.addEventListener("click", () => {
     console.log("Clicked");
 })
 
-document.addEventListener("keydown", (event) => {
-    console.log(event);
-});
-
-const follower = document.querySelector(".follower");
-
-document.addEventListener("mousemove", (event) => {
+document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
     const x = event.clientX;
     const y = event.clientY;
-    follower.style.left = x + "px";
-    follower.style.top = y + "px";
+    customContextMenu.style.top = y + "px";
+    customContextMenu.style.left = x + "px";
+    customContextMenu.style.display = "block";
+});
+
+customContextMenu.addEventListener("click", () => {
+    customContextMenu.style.display = "none";
+})
+
+document.addEventListener("mousemove", (event) => {
+    
+    // follower.style.display = "none"
 })
