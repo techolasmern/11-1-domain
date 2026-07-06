ede82b941ddd039a933698597765218bf75978bb
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-06 12:04:46 +0530

    media event & form event

--- JavaScript/04-07-2026/events.js
+++ JavaScript/04-07-2026/events.js
@@ -4,18 +4,50 @@ const usernameElement = document.getElementById("username");
 
 console.log(usernameElement.value);
 
-
 const form = document.getElementById("login");
 
 form.addEventListener("input", event => {
     // console.log(event.target.name, event.target.value);
 })
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
-    // const username = document.getElementById("username").value;
-    // const password = document.getElementById("password").value;
-    const username = document.frm.username.value;
-    const password = document.frm.password.value;
-    console.log(username, password)
+// form.addEventListener("submit", event => {
+//     event.preventDefault();
+//     // const username = document.getElementById("username").value;
+//     // const password = document.getElementById("password").value; // using id
+//     const username = document.frm.username.value;
+//     const password = document.frm.password.value; // using name
+//     console.log(username, password)
+// })
+
+// media events
+
+// play, pause, ended
+
+const video = document.getElementById("video");
+
+console.log(video);
+
+video.addEventListener("play", () => {
+    console.log("play");
 })
+
+video.addEventListener("pause", () => {
+    console.log("pause");
+})
+
+video.addEventListener("ended", () => {
+    console.log("ended");
+})
+
+// ---------------------------
+
+const handleChange = () => {
+    console.log("Change")
+}
+
+const handleSubmit = () => {
+    console.log("Submit")
+    return false;
+}
+
+
